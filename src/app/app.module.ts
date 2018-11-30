3906416f140fb469920556675265a2fbd2689164
Ankita Chavan <rh0225@accionlabs.in>
2018-11-30 18:06:14 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { EditshopComponent } from './Shopping-list/editshop/editshop.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailsComponent,
+    RecipesItemComponent,
+    ShoppingListComponent,
+    EditshopComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/editshop/editshop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editshop',
+  templateUrl: './editshop.component.html',
+  styleUrls: ['./editshop.component.css']
+})
+export class EditshopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
